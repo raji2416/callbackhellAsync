0f192c5cad520ae8d936ddfef9231a0b74d639ed
raji2416 <rajiraruthra24@gmail.com>
2023-11-17 16:26:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+setTimeout(() =>{
+    console.log(10);
+},2);
+setTimeout(() =>{
+    console.log(9);
+},4);
+
+setTimeout(() =>{
+    console.log(8);
+},6);
+
+setTimeout(() =>{
+    console.log(7);
+},8);
+setTimeout(() =>{
+    console.log(6);
+},12);
+
+setTimeout(() =>{
+    console.log(5);
+},14);
+setTimeout(() =>{
+    console.log(4);
+},16);
+
+setTimeout(() =>{
+    console.log(3);
+},18);
+
+setTimeout(() =>{
+    console.log(2);
+},20);
+
+setTimeout(() =>{
+    console.log(1);
+},22);
+setTimeout(() =>{
+    console.log("Happy Independence Day");
+},23)
+
+
+
